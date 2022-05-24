cfe89ffc5d67d4844f031712dc91c7749e751960
PranavJ96 <pranavjayanayak@gmail.com>
2022-05-25 01:57:33 +0200

    Side Nav along with Tab functionality. Chat Screen, Profile screen added

--- src/components/Sidenav/Event/Event.js
+++ src/components/Sidenav/Event/Event.js
@@ -4,18 +4,40 @@ import './Event.css';
 
 const Event = () => {
   return (
-    <div className="sideNavEvent">
-        <div className='sideNavEventHeader'>
-            Coffee Date
+    <div className='sideNavContainer'>
+        <div className="sideNavWelcomeText">
+            Welcome To Events
         </div>
-        <div className="sideNavEventFooter">
-            <div>
-                Take me to your favorite cafe
+        <div className="sideNavEventsList">
+            <div className="sideNavEvent">
+                <div className='sideNavEventHeader'>
+                    Coffee Date
+                </div>
+                <div className="sideNavEventFooter">
+                    <div>
+                        Take me to your favorite cafe
+                    </div>
+                    <div>
+                        <Button className='sideNavEventJoinButton' size="medium">
+                        Join Now
+                        </Button>
+                    </div>
+                </div>
             </div>
-            <div>
-                <Button className='sideNavEventJoinButton' size="medium">
-                Join Now
-                </Button>
+            <div className="sideNavEventBlind">
+                <div className='sideNavEventHeader'>
+                    Blind Date
+                </div>
+                <div className="sideNavEventFooter">
+                    <div>
+                        Find a random match and see where it goes.
+                    </div>
+                    <div>
+                        <Button className='sideNavEventJoinButton' size="medium">
+                        Join Now
+                        </Button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
