0ac7524662f9b7834e457006b3237d1aa917ffed
Smruti Puranik <smrutip16@gmail.com>
2022-05-26 20:26:25 +0200

    Created and Routed the Register and Signin Page

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -3,16 +3,35 @@ import {Link} from 'react-router-dom';
 import {ToastContainer, toast} from 'react-toastify';
 
 export default function Register(){
-    return <div className="container">
+    const [values,setValues] = useState({
+        email:"",
+        password:"",
+    });
+
+    //prevent form submission
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    return ( 
+    <div className="container">
         <h2>Register Account</h2>
-        <form>
+        <form onSubmit={(e)=>handleSubmit(e)}>
             <div>
                 <label htmlFor="email">Email</label>
-                <input type="email" name="email" placeholder="Email"/>
-            </div>
+                <input 
+                type="email" 
+                name="email" 
+                placeholder="Email"
+                onChange={(e)=>setValues({...values, [e.target.name]: e.target.value})}/> 
+            </div> 
             <div>
                 <label htmlFor="password">Password</label>
-                <input type="password" name="password" placeholder="Password"/>
+                <input 
+                type="password" 
+                name="password" 
+                placeholder="Password" 
+                onChange={(e)=>setValues({...values, [e.target.name]: e.target.value})}/>
             </div>
             <button type="submit">Submit</button>
             <span>
@@ -21,4 +40,5 @@ export default function Register(){
         </form>
         <ToastContainer/>
     </div>
+    );
 }
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,28 +1,44 @@
-
 import React,{useState} from "react";
-import ReactDOM from 'react-dom';
+import {Link} from 'react-router-dom';
+import {ToastContainer, toast} from 'react-toastify';
 
 export default function Signin(){
-    return <div>Signin</div>
+    const [values,setValues] = useState({
+        email:"",
+        password:"",
+    });
+
+    //prevent form submission
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    return ( 
+    <div className="container">
+        <h2>Signin Account</h2>
+        <form onSubmit={(e)=>handleSubmit(e)}>
+            <div>
+                <label htmlFor="email">Email</label>
+                <input 
+                type="email" 
+                name="email" 
+                placeholder="Email"
+                onChange={(e)=>setValues({...values, [e.target.name]: e.target.value})}/> 
+            </div> 
+            <div>
+                <label htmlFor="password">Password</label>
+                <input 
+                type="password" 
+                name="password" 
+                placeholder="Password" 
+                onChange={(e)=>setValues({...values, [e.target.name]: e.target.value})}/>
+            </div>
+            <button type="submit">Submit</button>
+            <span>
+                New to this application? <Link to="/register">Register</Link>
+            </span>
+        </form>
+        <ToastContainer/>
+    </div>
+    );
 }
-/* <form action="http://localhost:5000/api/addprofile" method="POST" enctype="multipart/form-data">
-<div>
-    <label for="name">Image Title</label>
-    <input onChange={handleTitle} type="text" id="name" placeholder="Name" 
-            value={title} name="name" required/>
-</div>
-<div>
-    <label for="about">About</label>
-    <textarea onChange={handleDescription} id="about" name="about" value={description} rows="2" 
-                placeholder="Description" required>
-    </textarea>
-</div>
-<div>
-    <label for="image">Upload Image</label>
-    <input onChange={handleFile} type="file" id="image" 
-            name="image" value={file} required/>
-</div>
-<div>
-    <button type="submit">Submit</button>
-</div>
-</form> */
