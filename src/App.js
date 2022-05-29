bd8169147427c8a360ab8efd5671778d238d50e8
abdallah96 <gueye.amadou1996@@gmail.com>
2022-05-29 23:35:29 +0200

    Profile with questionnaires design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Test from './components/Test/Test';
+import Questionnaire from './components/Questionnaire/Questionnaire';
+import ProfileDetails from './components/ProfileDetails/ProfileDetails';
 function App() {
   return (
     <div className="App">
-      <Test/>
+      {/* <Questionnaire/> */}
+      {/* <Test/> */}
+      <ProfileDetails/> */
     </div>
   );
 }
