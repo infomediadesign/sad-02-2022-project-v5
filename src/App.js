a23b1390f38be42cde229c21f383da6051b861c5
abdallah96 <gueye.amadou1996@@gmail.com>
2022-06-01 23:20:26 +0200

    SignUp Datas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 import Admin from './components/Admin/Admin';
+import SignUp from './components/SignUp/SignUp';
+import Profile from './components/Profile/Profile';
+import Sidenav from './components/Sidenav/Sidenav';
+import ProfileDetails from './components/ProfileDetails/ProfileDetails';
 function App() {
   return (
     <div className="App">
-      <Admin/>
+      <SignUp/>
+      {/* <ProfileDetails/> */}
     </div>
   );
 }
