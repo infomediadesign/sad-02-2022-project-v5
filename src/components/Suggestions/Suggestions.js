fc5e7a91e4cf7562d3d882ffd4d9060b5a250a7d
shubhamc61 <shubhamc61@icloud.com>
2022-06-03 15:05:09 +0200

    merging development for getting location data

--- src/components/Suggestions/Suggestions.js
+++ src/components/Suggestions/Suggestions.js
@@ -31,11 +31,35 @@ const Suggestions = () => {
                 
                     setData(response.data.data[1]);
                     setAge(getAge(response.data.data[1].dob))
+                    // getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2)
                     console.log(response.data.data[1].dob)
                     console.log(age)
                     })
               });
      
+            
+              console.log(getDistanceFromLatLonInKm(59.3293371,13.4877472,59.3225525,13.4619422).toFixed(1));
+              
+
+
+                function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+                var R = 6371; // Radius of the earth in km
+                var dLat = deg2rad(lat2-lat1);  // deg2rad below
+                var dLon = deg2rad(lon2-lon1); 
+                var a = 
+                    Math.sin(dLat/2) * Math.sin(dLat/2) +
+                    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+                    Math.sin(dLon/2) * Math.sin(dLon/2)
+                    ; 
+                var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+                var d = R * c; // Distance in km
+                return d;
+                }
+
+                function deg2rad(deg) {
+                return deg * (Math.PI/180)
+                }
+
 
 
             function getAge(dateString) {
