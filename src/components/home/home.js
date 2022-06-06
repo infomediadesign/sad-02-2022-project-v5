fc9cc908740f9990d7f148e53335a486fc306be5
shubhamc61 <shubhamc61@icloud.com>
2022-06-06 13:46:55 +0200

    taking userid from cookies and showing profile according to credentials

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -10,6 +10,7 @@ const Home = () => {
     const navigate = useNavigate();
   const [cookies, setCookie, removeCookie] = useCookies([]);
   useEffect(() => {
+    console.log(cookies.userid)
     const verifyUser = async () => {
       if (!cookies.jwt) {
           console.log("jwt does not exist")
