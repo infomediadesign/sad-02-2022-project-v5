0dda462972ce17da9fe876f8bdc750ed556c3b80
Harshuj4 <Harshinij46@gmail.com>
2022-06-07 23:59:25 +0200

    Modified chat and chat details code to retrieve the messages

--- src/components/chat/ChatDetails/ChatDetails.js
+++ src/components/chat/ChatDetails/ChatDetails.js
@@ -1,96 +1,52 @@
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 import './ChatDetails.css'
 import {Avatar, IconButton} from '@material-ui/core'
 import { SearchOutlined } from '@mui/icons-material';
 import { CallOutlined} from '@material-ui/icons';
 import { InsertEmoticonOutlined } from '@material-ui/icons';
 import axios from 'axios';
-import ChatList from '../ChatList/ChatList';
 import SidebarChatList from '../SidebarChatList/SidebarChatList';
 
-const ChatDetails = () => {
-  const [data,setData] = useState([]);
-  const [UserName,setUserName] = useState("");
-  const [input,setInput] = useState("");
-  var receivedMessages;
-  var sentMessages;
-  var tempMessages = [];
-  var mydata = {
-      myid:"pranav@gmail.com",
-  } 
-
-    axios.get('http://localhost:5000/api/getmymessages/',{ params: data}).then((response)=>{
-       console.log(response.data);
-       if(response.data !== undefined)
-       {
-         for(var i=0;i<response.data.length;i++){
-          if(response.data[i].members[i] === mydata.myid){
-            receivedMessages = {
-            rmessage:response.data[i].messages[i].text
-          }
-         }
-         else
-         {
-          sentMessages = {
-            smessage:response.data[i].messages[i].text
-          }
-         }
-         tempMessages.push(receivedMessages,sentMessages);
-         }
-         setData(tempMessages);
-       } 
-
-      //  const handleUserName = () => {
-      //    setUserName(e.target.value);
-      //  }
-
-      //  const sendMessage = (e) => {
-      //     e.preventDefault();
-      //     console.log(response,input);
-      //  }
-      });
+const ChatDetails = ({userName,senderMessage,receiverMessage}) => {
 
   return (
     <div className='chat_details'>
       <div className='chatdetails_header'>
         <Avatar />
 
-        <div className='chatdetails_headerInfo'>
-        { data.map((object, i) => <SidebarChatList UserName = {(object.name) }/>)}
+         <div className='chatdetails_headerInfo'>
+          <h3>{userName}</h3>
           <div className='chatheader_timestamp'>
           <p>Last seen at: </p>
           <span>{new Date().toUTCString()}</span>
           </div>
         </div>
-        
+         
         <div className='chatdetails_headeronright'>
-        <IconButton>
-            <SearchOutlined/>
-          </IconButton>
-          <IconButton>
-            <CallOutlined/>
-          </IconButton>
+
+
         </div>
       </div>
 
-      <div className='chatdetails_body'>
-        <p className='chatmessage'>{receivedMessages}
+       <div className='chatdetails_body'>
+        <p className='chatmessage'>{senderMessage}
         </p>
 
-        <p className='chatmessage chat_received'>{sentMessages}
+        <p className='chatmessage chat_received'>{receiverMessage}
         </p>
         </div>
 
-      <div className='chatdetails_footer'>
+      {/* <div className='chatdetails_footer'>
         <InsertEmoticonOutlined/>
         <form>
-        <input 
-          placeholder="Type a message" onChange={(e) => setInput(e.target.value)}
+        <input value={input}
+          placeholder="Type a message" 
+          onChange={(e) => setInput(e.target.value)}
           type="text"/>
-           <button className='sendbutton' value={sentMessages}
+           <button className='sendbutton' onClick={sendMessage}
             type="submit">Send</button>
            </form>
-      </div>
+      </div>  */}
     </div>
   );
 };
--- src/components/chat/chat.js
+++ src/components/chat/chat.js
@@ -1,15 +1,76 @@
-import React from 'react'
 import './Chat.css'
-import ChatList from './ChatList/ChatList'
 import ChatDetails from './ChatDetails/ChatDetails'
+import React, { useEffect, useState } from 'react'
+import { SearchOutlined } from '@mui/icons-material';
+import SidebarChatList from './SidebarChatList/SidebarChatList';
+import axios from 'axios';
 
 function Chat (){
+  const[data,setData] = useState([]);
+  const[chatData,setChatData] = useState([]);
+  var userInfo;
+  var tempdata = [];
+  var mydata = {
+    myid:"pranav@gmail.com"
+    
+    }
+    useEffect(() =>{
+      axios.get('http://localhost:5000/api/getmymessages/',{ params: mydata}).then((response)=>{
+        console.log(response.data);
+       if(response.data !== undefined){
+        for(var i = 0; i< response.data.length; i++){
+        if(response.data[i].members[0] !== mydata.myid){
+          if(response.data[i].members[i].receiverid !== mydata.myid)
+          userInfo = {
+               name:response.data[i].members[0],
+               smessage:response.data[i].messages[i].text,
+          }
+        }
+        else{
+          if(response.data[i].members[i].senderid !== mydata.myid)
+          userInfo = {
+               name:response.data[i].members[1],
+               rmessage:response.data[i].messages[i].text,
+               
+          }
+        }
+        tempdata.push(userInfo);
+      }
+      setData(tempdata);
+    }
+      });
+    },[])
+
+  
+    const getChatDetails= (data) =>{
+      debugger
+      setChatData(data);
+    }
+    console.log(chatData);
    return (
     <div className='chatContainer'>
       <div className='chatcontainer_body'>
-      <ChatList/>
-      <ChatDetails/>
-      </div>
+      <div className='chat_list'>
+    {/* <div className='chatlistbar_header'>
+        <div className='Chatlist_searchbar'>
+            <div className='chatlist_searchbarcontainer'>
+                <SearchOutlined/>
+                <input placeholder='Search or start new chat' type="text"/>
+            </div>
+        </div>
+    </div> */}
+    <div className='chatLists_Container'>
+    { data.map((object, i) => <ChatDetails userName = {(object.name)} senderMessage = {(object.smessage)} receiverMessage ={(object.rmessage)} />)} 
+    { data.map((object, i) => <div onClick={()=>getChatDetails(object)} className='sidebarchat_container'>
+        <div className='sidebarchat_info'>
+          <h3>{object.name}</h3>  
+        </div>
+    </div>)}
+     
+    </div>
+</div> 
+
+       </div>
       </div>
   )
 }
