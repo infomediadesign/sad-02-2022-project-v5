bdd3aa4a2284742827fe3cf0c3df4e2a37a53385
PranavJ96 <pranavjayanayak@gmail.com>
2022-06-08 00:21:22 +0200

    blind date functionality added

--- backend/routes/events.js
+++ backend/routes/events.js
@@ -10,13 +10,32 @@ module.exports = function(app) {
         var myData;
         var myId = req.body.myid;
         var blindData;
+        var myblindData;
         var blindDateProfile;
         var genderPrefference
-        console.log(req.body.myid)
-        myData = await userProfile.findOne({userid:req.body.myid}).select({ "_id": 0}).clone();
+        myData = await userProfile.findOne({userid:req.body.myid}).clone();
         blindData = await  blindProfile.findOne({userid:req.body.myid}).select({ "userid": 1}).clone();
         if(!blindData){
-            await blindProfile.create(myData)
+            var blindObj = {
+                name: myData.name,
+                userid: myData.userid,
+                about: myData.about,
+                location: myData.location,
+                findwithin:myData.findwithin,
+                passion:myData.passion,
+                bestdrink:myData.bestdrink,
+                education:myData.education,
+                foodpreferences:myData.foodpreferences,
+                bestpets:myData.bestpets,
+                smoking:myData.smoking,
+                Socialmedia:myData.Socialmedia,
+                gender:myData.gender,
+                preferredgender:myData.preferredgender,
+                dob:myData.dob,
+                img: myData.img
+            }
+            await blindProfile.create(blindObj)
+            myblindData = await blindProfile.findOne({userid:req.body.myid}).select({"userid":1,"matches":1,"liked":1})
         }
         if(myData.preferredgender === 'everyone'){
             genderPrefference = ['man','everyone','woman']
@@ -40,10 +59,10 @@ module.exports = function(app) {
         if(blindDateProfile){
             blindDateProfile.matches.push(req.body.myid)
             blindDateProfile.liked.push(req.body.myid)
-            myData.matches.push(blindDateProfile.userid);
-            myData.liked.push(blindDateProfile.userid);
-            await userProfile.findOneAndUpdate({$and:[{userid:blindDateProfile.userid},{matches:{$ne:req.body.likedData.myid }}]},blindDateProfile).clone()
-            await userProfile.findOneAndUpdate({$and:[{userid: req.body.likedData.myid},{liked:{$ne:req.body.likedData.profileid }},{matches:{$ne:req.body.likedData.profileid }}]},myData).clone()
+            myblindData.matches.push(blindDateProfile.userid);
+            myblindData.liked.push(blindDateProfile.userid);
+            await blindProfile.findOneAndUpdate({$and:[{userid:blindDateProfile.userid},{liked:{$ne:req.body.myid }},{matches:{$ne:req.body.myid }}]},blindDateProfile).clone()
+            await blindProfile.findOneAndUpdate({$and:[{userid: req.body.myid},{liked:{$ne:blindDateProfile.userid }},{matches:{$ne:blindDateProfile.userid }}]},myblindData).clone()
             var obj = {members:[req.body.myid,blindDateProfile.userid],messages:[]}
             var createdAlready = await conversation.findOne({$and:[{members:blindDateProfile.userid},{members:req.body.myid }]})
             if(!createdAlready){await conversation.create(obj);}
