50a4545e336e1d2ed25223cbb283e8e68f340956
Amadou <amadou@future-stories.com>
2022-06-13 02:40:54 +0200

    testing2

--- frontend/src/components/Chat/Chat.test.js
+++ frontend/src/components/Chat/Chat.test.js
@@ -1,9 +1,17 @@
 import { shallow } from "enzyme";
 import {ToastContainer} from 'react-toastify';
+import Chat from "./Chat";
 import DateSuggestions from "./DateSuggestions/DateSuggestions";
 
 describe("MyProfile", () => {
     let wrapper = null;
 
-    
+    beforeEach(() => {
+        wrapper = shallow(<Chat/>);
+      });
+
+    it('check if the add admin text is the same', () => {
+        let header = wrapper.find(".sendbutton").text();
+        expect(header).toBe("Send");
+      });
 })
