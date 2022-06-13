e30d10a3f880bd95790c38072968f023c2a17977
Amadou <amadou@future-stories.com>
2022-06-13 02:27:11 +0200

    testing1

---
+++ frontend/src/components/Chat/Chat.test.js
@@ -0,0 +1,9 @@
+import { shallow } from "enzyme";
+import {ToastContainer} from 'react-toastify';
+import DateSuggestions from "./DateSuggestions/DateSuggestions";
+
+describe("MyProfile", () => {
+    let wrapper = null;
+
+    
+})
